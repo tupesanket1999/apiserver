6c82c2f64e71ac7d8ac67b4fe6537a39bd91141e
stupe <stupe@uptycs.com>
2022-04-14 04:33:12 +0530

    cred added

--- database/credentials.js
+++ database/credentials.js
@@ -1,7 +1,8 @@
-const user = "postgres";
-const host = "localhost";
-const database = "bluesky";
-const password = "bluesky";
+const user = "ioewaoeteoyflr";
+const host = "ec2-34-207-12-160.compute-1.amazonaws.com";
+const database = "dcjnjf5p6dn44o";
+const password =
+  "31ae3790be9e3ef03f3e473a8547c6d94fa30c03024a607ebeb1cff92c100e6e";
 const port = "5432";
 module.exports = {
   user,
